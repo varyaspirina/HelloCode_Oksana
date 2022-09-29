a586142127df4c3ec8e5331648ebaea60d6d6f62
Oksana_P <popovtseva.o.n@yandex.ru>
2022-09-29 13:40:09 +0500

    Add special greeting for Oksana

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter USER name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "oksana")
+{
+    Console.WriteLine("Hello, Oksana! It's a great pleasure to see you again.");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
