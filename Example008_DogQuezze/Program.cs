8a8b478d73c27222876b48bbd1814f41838d3f61
Oksana_P <popovtseva.o.n@yandex.ru>
2022-09-30 19:01:09 +0500

    Add Example008_DogQuezze

---
+++ Example008_DogQuezze/Program.cs
@@ -0,0 +1,27 @@
+﻿double FirstFriendSpeed = 1;
+double SecondFriendSpeed = 4;
+double DogSpeed = 3;
+double Distance = 80000;
+
+int count = 0;
+int friend = 2;
+double time = 0;
+
+while (Distance > 10)
+{
+    if (friend == 1)
+    {
+        time = Distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = Distance / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+    Distance = Distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine ("Колическо раз, что пробежала собака:");
+Console.WriteLine (count);
